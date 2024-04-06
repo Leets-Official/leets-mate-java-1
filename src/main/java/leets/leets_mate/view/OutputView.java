623f8378c556deeeacff7b8558c7b972a0692259
ay-eonii <naayen@naver.com>
2024-04-07 00:53:38 +0900

    feat(OutputView): 매칭 시작 문구 출력

---
+++ src/main/java/leets/leets_mate/view/OutputView.java
@@ -0,0 +1,9 @@
+package leets.leets_mate.view;
+
+public class OutputView {
+
+    public void printStartNotice() {
+        System.out.println("[Leets 오늘의 짝에게]를 시작합니다.");
+        System.out.println();
+    }
+}

fab7a8e1d0000f3d377d6007bb40b3fb6ace8966
ay-eonii <naayen@naver.com>
2024-04-06 20:32:48 +0900

    feat(Name): 이름 도메인

---
+++ src/main/java/leets/leets_mate/Name.java
@@ -0,0 +1,19 @@
+package leets.leets_mate;
+
+public class Name {
+
+    private static final int MAX_LENGTH = 5;
+
+    private final String name;
+
+    public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/leets/leets_mate/NameTest.java
@@ -0,0 +1,16 @@
+package leets.leets_mate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class NameTest {
+
+    @Test
+    @DisplayName("이름의 길이가 6글자 이상이라면 예외가 발생한다.")
+    void name_InvalidLength_ExceptionThrown() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Name("나아연이라능"));
+    }
+}

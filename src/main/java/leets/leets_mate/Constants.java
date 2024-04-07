8f5119e0049309e85010506f4ae4a3bd61db69ad
노정완 <jeongwan.noh@gmail.com>
2024-04-07 17:30:31 +0900

    feat: 동아리원 이름이 영어인 예외처리 구현

--- src/main/java/leets/leets_mate/Constants.java
+++ src/main/java/leets/leets_mate/Constants.java
@@ -1,12 +1,15 @@
 package leets.leets_mate;
 
 public enum Constants {
+    NEWLINE("\n"),
     INTRO("Leets [오늘의 짝에게]를 시작합니다!"),
     INPUT("동아리원의 이름을 ','로 구분하여 한글로 입력해주세요!"),
     MATE("최대 짝(그룹 수)를 입력해주세요! 단, 최대 짝은 동아리원의 수보다 작거나 같습니다."),
     RESULT("오늘의 짝 추천 결과입니다."),
     CONTINUE("추천을 완료했습니다.\n" + "다시 구성하시겠습니까? (y or n"),
-    HELLO("자리를 이동해 서로에게 인사해주세요");
+    HELLO("자리를 이동해 서로에게 인사해주세요"),
+    ERROR_INPUT("잘못된 입력입니다. 다시 입력해주세요.");
+
     private final String message;
 
     public String getMessage() {
--- src/main/java/leets/leets_mate/LeetsMateApplication.java
+++ src/main/java/leets/leets_mate/LeetsMateApplication.java
@@ -1,16 +1,38 @@
 package leets.leets_mate;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static leets.leets_mate.Constants.*;
 
 public class LeetsMateApplication {
 
     // 동작 함수입니다.
-    public void run() {
+    public void run() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(INTRO.getMessage());
+        System.out.print(NEWLINE.getMessage());
+        System.out.println(INPUT.getMessage());
+
+        String members = br.readLine();
+        try {
+            checkHasNoEnglish(members);
+        } catch (Exception e) {
+            System.out.println(ERROR_INPUT.getMessage());
+            run();
+        }
+
+        List<String> parsedMembers = parseMembers(members);
     }
 
     // 문자열로된 멤버들을 리스트로 분리하는 함수입니다.
     public List<String> parseMembers(String members) {
-        return new ArrayList<>();
+        String[] arr = members.split(",");
+        return Arrays.stream(arr).toList();
     }
 
     // 총 멤버수를 반환합니다.
@@ -20,6 +42,13 @@ public class LeetsMateApplication {
 
     // 멤버 문자열에 영어가 있는지 검사합니다. 영어가 있다면 예외 출력
     public void checkHasNoEnglish(String members) {
+        String regex = ".*[a-zA-Z].*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(members);
+
+        if (matcher.matches()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     // 멤버수와 최대 짝수 데이터가 유효한지 검사하는 함수입니다. 유효하지 않다면 예외 출력
@@ -35,7 +64,7 @@ public class LeetsMateApplication {
     public void printResult(List<List<String>> result) {
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         LeetsMateApplication app = new LeetsMateApplication();
         app.run();
     }

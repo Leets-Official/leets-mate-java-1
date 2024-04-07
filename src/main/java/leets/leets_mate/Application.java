430f2c1a86e7998988accee01e2857ab4308f006
ay-eonii <naayen@naver.com>
2024-04-07 23:53:00 +0900

    feat(Applications): 컨트롤러 호출

---
+++ src/main/java/leets/leets_mate/Application.java
@@ -0,0 +1,10 @@
+package leets.leets_mate;
+
+import leets.leets_mate.controller.Computer;
+
+public class Application {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        computer.run();
+    }
+}

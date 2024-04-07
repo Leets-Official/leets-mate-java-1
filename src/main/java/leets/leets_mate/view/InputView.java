b7a3a87f2279138350557c2d639e78e3a828b4f5
ay-eonii <naayen@naver.com>
2024-04-07 23:39:07 +0900

    feat(InputView): 최대 짝 수 입력받는 메서드

--- src/main/java/leets/leets_mate/view/InputView.java
+++ src/main/java/leets/leets_mate/view/InputView.java
@@ -12,17 +12,35 @@ public class InputView {
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> readNames() {
+        System.out.println();
+        System.out.println("멤버의 이름을 입력해 주세요. (, 로 구분)");
+
         String input = scanner.nextLine();
         List<String> rawNames = List.of(input.split(COMMA, -1));
-        for (String rawName : rawNames) {
-            validateInput(rawName);
+        try {
+            rawNames.forEach(this::validateInput);
+            return rawNames;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readNames();
         }
-        return rawNames;
     }
 
     private void validateInput(String rawName) {
         if (!FORMAT.matcher(rawName).matches()) {
             throw new IllegalArgumentException("한글만 입력해 주세요");
+        }
+    }
+
+    public int readPairCount() {
+        System.out.println();
+        System.out.println("최대 짝 수를 입력해 주세요.");
+
+        String input = scanner.nextLine();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해 주세요");
         }
     }
 }

87c7207a21f4554b4b06e631c8a29b72a2f57e5d
jiixon <jr0107380@gmail.com>
2024-04-09 18:20:31 +0900

    :sparkles:[feat]:parseMembers 기능구현

--- src/main/java/leets/leets_mate/LeetsMateApplication.java
+++ src/main/java/leets/leets_mate/LeetsMateApplication.java
@@ -30,7 +30,7 @@ public class LeetsMateApplication {
 
     // 문자열로된 멤버들을 리스트로 분리하는 함수입니다.
     public List<String> parseMembers(String members) {
-        return new ArrayList<>();
+        return List.of(members.split(","));
     }
 
     // 총 멤버수를 반환합니다.

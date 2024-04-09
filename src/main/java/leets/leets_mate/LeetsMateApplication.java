c0fd34088fcb7a9bcbf9ccdcaf2af3fbc90c6741
unknown <leejh5838@naver.com>
2024-04-09 23:54:19 +0900

    test_1

--- src/main/java/leets/leets_mate/LeetsMateApplication.java
+++ src/main/java/leets/leets_mate/LeetsMateApplication.java
@@ -3,9 +3,17 @@ package leets.leets_mate;
 import java.util.*;
 
 public class LeetsMateApplication {
-
+    Scanner scanner = new Scanner(System.in);
     // 동작 함수입니다.
     public void run() {
+        System.out.println("[Leets 오늘의 짝에게]를 시작합니다.");
+        System.out.println("멤버의 이름을 입력해 주세요. (, 로 구분) ");
+        String memberCount = scanner.nextLine();
+        List<String> memberList = new ArrayList<>();
+        Collections.addAll(memberList, memberCount.split(","));
+
+        System.out.println("최대 짝 수를 입력해 주세요.");
+        int maximumGroupSize = scanner.nextInt();
     }
 
     // 문자열로된 멤버들을 리스트로 분리하는 함수입니다.
@@ -20,10 +28,16 @@ public class LeetsMateApplication {
 
     // 멤버 문자열에 영어가 있는지 검사합니다. 영어가 있다면 예외 출력
     public void checkHasNoEnglish(String members) {
+        if (!members.matches("[가-힣, ]+")) {
+            throw new IllegalArgumentException("[ERROR] 이름은 한글로 입력해야 합니다");
+        }
     }
 
     // 멤버수와 최대 짝수 데이터가 유효한지 검사하는 함수입니다. 유효하지 않다면 예외 출력
     public void checkDataValidity(int memberCount, int maximumGroupSize) {
+        if (maximumGroupSize > memberCount.size()) {
+            throw new IllegalArgumentException("[ERROR] 최대 짝 수는 이름의 갯수보다 클 수 없습니다");
+        }
     }
 
     // 랜덤 짝꿍 추첨하는 함수 입니다.
@@ -33,6 +47,13 @@ public class LeetsMateApplication {
 
     // 결과를 프린트 하는 함수입니다.
     public void printResult(List<List<String>> result) {
+        System.out.println("오늘의 짝 추천 결과입니다.");
+
+        System.out.print("다시 구성하시겠습니까? (y or n): ");
+        String answer = scanner.nextLine();
+        if (answer.equalsIgnoreCase("n")) {
+            break;
+        }
     }
 
     public static void main(String[] args) {

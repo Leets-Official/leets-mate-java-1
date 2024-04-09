47a872835e560952adcf02918906ba9600f419c0
jiixon <jr0107380@gmail.com>
2024-04-09 18:19:32 +0900

    :sparkles:[feat]: run()함수 및 입력값 구현

--- src/main/java/leets/leets_mate/LeetsMateApplication.java
+++ src/main/java/leets/leets_mate/LeetsMateApplication.java
@@ -3,9 +3,29 @@ package leets.leets_mate;
 import java.util.*;
 
 public class LeetsMateApplication {
+    static String members;
+    static List<String> memberList;
 
     // 동작 함수입니다.
-    public void run() {
+    public void run() throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("[Leets 오늘의 짝에게]를 시작합니다.\n");
+        System.out.println("멤버의 이름을 입력해 주세요. (, 로 구분)");
+        members = sc.nextLine();
+
+        checkHasNoEnglish(members);
+
+        System.out.println();
+
+        System.out.println("최대 짝 수를 입력해 주세요.");
+        int max = sc.nextInt();
+
+        memberList = parseMembers(members);
+
+        System.out.println("오늘의 짝 추천 결과입니다.\n");
+
+
     }
 
     // 문자열로된 멤버들을 리스트로 분리하는 함수입니다.
@@ -19,7 +39,7 @@ public class LeetsMateApplication {
     }
 
     // 멤버 문자열에 영어가 있는지 검사합니다. 영어가 있다면 예외 출력
-    public void checkHasNoEnglish(String members) {
+    public void checkHasNoEnglish(String members) throws Exception {
     }
 
     // 멤버수와 최대 짝수 데이터가 유효한지 검사하는 함수입니다. 유효하지 않다면 예외 출력
@@ -35,7 +55,7 @@ public class LeetsMateApplication {
     public void printResult(List<List<String>> result) {
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         LeetsMateApplication app = new LeetsMateApplication();
         app.run();
     }

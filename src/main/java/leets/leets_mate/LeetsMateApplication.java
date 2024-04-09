15349380e87f8d4d9d2890b62f0b1bc80a6449ec
ay-eonii <naayen@naver.com>
2024-04-09 22:25:31 +0900

    fix(Application): 메인 메서드 중복 삭제

--- src/main/java/leets/leets_mate/LeetsMateApplication.java
+++ src/main/java/leets/leets_mate/LeetsMateApplication.java
@@ -1,8 +1,11 @@
 package leets.leets_mate;
 
+import leets.leets_mate.controller.Computer;
+
 public class LeetsMateApplication {
 
     public static void main(String[] args) {
-
+        Computer computer = new Computer();
+        computer.run();
     }
 }

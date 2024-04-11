f665a22fc01756051dce8983a6c19088a6312c63
DongHoon Lee <dhl1924@naver.com>
2024-04-11 14:49:46 +0900

    feat(OutputView): 최대 짝수 입력 멘트를 출력하는 메서드 구현

--- src/main/java/leets/leets_mate/view/OutputView.java
+++ src/main/java/leets/leets_mate/view/OutputView.java
@@ -1,5 +1,7 @@
 package leets.leets_mate.view;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.List;
 
 public class OutputView {
@@ -7,6 +9,10 @@ public class OutputView {
         System.out.println("[Leets 오늘의 짝에게]를 시작합니다.");
         System.out.println();
         System.out.println("멤버의 이름을 입력해 주세요. (, 로 구분)");
+    }
+
+    public static void printMaxGroup() {
+        System.out.println("최대 짝 수를 입력해 주세요.");
     }
     // 결과를 프린트 하는 함수입니다.
     public void printResult(List<List<String>> result) {

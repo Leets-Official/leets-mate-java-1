1a91a77a98898ab3315f9588dcc2a4f773085d55
DongHoon Lee <dhl1924@naver.com>
2024-04-11 09:45:41 +0900

    feat(OutputVIew): 결과를 프린트하는 뷰 생성

---
+++ src/main/java/leets/leets_mate/view/OutputVIew.java
@@ -0,0 +1,10 @@
+package leets.leets_mate.view;
+
+import java.util.List;
+
+public class OutputVIew {
+    // 결과를 프린트 하는 함수입니다.
+    public void printResult(List<List<String>> result) {
+    }
+
+}

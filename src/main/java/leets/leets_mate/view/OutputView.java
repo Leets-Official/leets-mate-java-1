e67a4a25c36949dd50b9d49ff70553ae72331eb3
DongHoon Lee <dhl1924@naver.com>
2024-04-11 10:32:59 +0900

    feat(OutputView): 시작 메시지를 출력하는 메서드 구현

---
+++ src/main/java/leets/leets_mate/view/OutputView.java
@@ -0,0 +1,15 @@
+package leets.leets_mate.view;
+
+import java.util.List;
+
+public class OutputView {
+    public void printStartMessage() {
+        System.out.println("[Leets 오늘의 짝에게]를 시작합니다.");
+        System.out.println();
+        System.out.println("멤버의 이름을 입력해 주세요. (, 로 구분)");
+    }
+    // 결과를 프린트 하는 함수입니다.
+    public void printResult(List<List<String>> result) {
+    }
+
+}

1d5e5165d9f589c120d19fe07101dedc84dece8f
DongHoon Lee <dhl1924@naver.com>
2024-04-11 11:20:31 +0900

    feat(MatchingController): 출력메시지, 멤버의 이름 입력받는 기능 컨트롤러에 구현

--- src/main/java/leets/leets_mate/controller/MatchingController.java
+++ src/main/java/leets/leets_mate/controller/MatchingController.java
@@ -1,8 +1,14 @@
 package leets.leets_mate.controller;
 
+import leets.leets_mate.view.InputView;
+import leets.leets_mate.view.OutputView;
+
+import java.util.Scanner;
+
 public class MatchingController {
+
     public void run() {
+        OutputView.printStartMessage();
+        InputView.readMember();
     }
-
-
 }

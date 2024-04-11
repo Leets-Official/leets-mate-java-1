da4b742942dd8bb2523b317b27fbb65a9dbdfa25
DongHoon Lee <dhl1924@naver.com>
2024-04-11 15:27:10 +0900

    feat(Member): 도메인 구현

--- src/main/java/leets/leets_mate/domain/Member.java
+++ src/main/java/leets/leets_mate/domain/Member.java
@@ -1,16 +1,32 @@
 package leets.leets_mate.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Member {
+    private String name;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
     // 문자열로된 멤버들을 리스트로 분리하는 함수입니다.
-    public List<String> parseMembers(String members) {
-        return new ArrayList<>();
+    public static List<Member> parseMembers(String[] memberName) {
+        List<Member> members = new ArrayList<>();
+        for (String name : memberName) {
+            members.add(new Member(name));
+        }
+
+        return members;
     }
 
     // 총 멤버수를 반환합니다.
-    public int memberNumber(List<String> members) {
-        return 0;
+    public static int memberNumber(List<Member> members) {
+        return members.size();
+    }
+
+    public String getName() {
+        return name;
     }
 }

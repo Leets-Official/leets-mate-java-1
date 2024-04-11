4cf6b38e6a6c433d71943b21ea7c574cd59ab879
DongHoon Lee <dhl1924@naver.com>
2024-04-11 09:45:27 +0900

    feat(InputVIew): 멤버 이름, 최대 짝 수를 입력받는 뷰 생성

---
+++ src/main/java/leets/leets_mate/view/InputVIew.java
@@ -0,0 +1,12 @@
+package leets.leets_mate.view;
+
+public class InputVIew {
+
+    // 멤버 문자열에 영어가 있는지 검사합니다. 영어가 있다면 예외 출력
+    public void checkHasNoEnglish(String members) {
+    }
+
+    // 멤버수와 최대 짝수 데이터가 유효한지 검사하는 함수입니다. 유효하지 않다면 예외 출력
+    public void checkDataValidity(int memberCount, int maximumGroupSize) {
+    }
+}

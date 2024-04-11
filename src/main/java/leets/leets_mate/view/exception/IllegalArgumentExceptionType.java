b6f215821c5c9dcfbbb75b896e357f1be0e5c9b4
DongHoon Lee <dhl1924@naver.com>
2024-04-11 17:31:05 +0900

    feat(IllegalArgumentExceptionType): retry 시 잘못된 입력 에러 상수 추가

--- src/main/java/leets/leets_mate/view/exception/IllegalArgumentExceptionType.java
+++ src/main/java/leets/leets_mate/view/exception/IllegalArgumentExceptionType.java
@@ -4,7 +4,8 @@ public enum IllegalArgumentExceptionType implements ExceptionType<IllegalArgumen
 
     INVALID_NAMING("[ERROR] 이름은 한글로 입력해야 합니다"),
     GROUP_SIZE_EXCEEDS_MEMBER_COUNT("[ERROR] 최대 짝 수는 이름의 갯수보다 클 수 없습니다."),
-    INVALID_MAX_GROUP("[ERROR] 최대 짝수는 숫자로 입력해야 합니다");
+    INVALID_MAX_GROUP("[ERROR] 최대 짝수는 숫자로 입력해야 합니다"),
+    IVALID_RETRY_INPUT("[ERROR] y와 n만 입력이 가능합니다.");
 
     private final String message;
 

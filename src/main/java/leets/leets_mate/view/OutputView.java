539d2942aa76f0df419f7407e885fc43da03999d
DongHoon Lee <dhl1924@naver.com>
2024-04-11 17:09:49 +0900

    feat(OutputView): Member 객체를 문자열로 변환하여 String.join 도입
    - 객체를 결합할 수는 없기에 String으로 타입을 변환해야 했다.

--- src/main/java/leets/leets_mate/view/OutputView.java
+++ src/main/java/leets/leets_mate/view/OutputView.java
@@ -1,8 +1,10 @@
 package leets.leets_mate.view;
 
+import leets.leets_mate.domain.Member;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
     public static void printStartMessage() {
@@ -12,10 +14,21 @@ public class OutputView {
     }
 
     public static void printMaxGroup() {
+        System.out.println();
         System.out.println("최대 짝 수를 입력해 주세요.");
     }
-    // 결과를 프린트 하는 함수입니다.
-    public void printResult(List<List<String>> result) {
-    }
 
+    // 결과를 프린트 하는 함수입니다.
+    public static void printResult(List<List<Member>> result) {
+        System.out.println("오늘의 짝 추천 결과입니다.");
+        for (List<Member> group : result) {
+
+            List<String> memberNames = group.stream()
+                    .map(Member::getName)
+                    .collect(Collectors.toList());
+            String groupResult = String.join(" | ", memberNames);
+
+            System.out.println("[ " + groupResult + " ]");
+        }
+    }
 }

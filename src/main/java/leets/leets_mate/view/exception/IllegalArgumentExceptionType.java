f168091fa53cfc7177eee6c7d4c33784beba6a1c
DongHoon Lee <dhl1924@naver.com>
2024-04-11 10:30:55 +0900

    feat(IllegalArgumentExceptionType): ExceptionType 인터페이스를 구현한 열거형 생성
    - 유효하지 않은 입력에 대한 예외 메시지
    - IllegalArgumentException 객체를 제공

---
+++ src/main/java/leets/leets_mate/view/exception/IllegalArgumentExceptionType.java
@@ -0,0 +1,23 @@
+package leets.leets_mate.view.exception;
+
+public enum IllegalArgumentExceptionType implements ExceptionType<IllegalArgumentException> {
+
+    INVALID_NAMING("[ERROR] 이름은 한글로 입력해야 합니다"),
+    GROUP_SIZE_EXCEEDS_MEMBER_COUNT("[ERROR] 최대 짝 수는 이름의 갯수보다 클 수 없습니다.");
+
+    private final String message;
+
+    IllegalArgumentExceptionType(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public IllegalArgumentException getException() {
+        return new IllegalArgumentException(getMessage());
+    }
+}

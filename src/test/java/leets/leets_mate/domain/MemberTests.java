615d27d99ed431671178307dfd998f1692aab318
DongHoon Lee <dhl1924@naver.com>
2024-04-11 15:28:41 +0900

    test(MemberTests): 타입 변환에 의한에 테스트 코드 수정
    - ArrayList의 제네릭 타입을 Member 클래스로 변환한 것에 대한 수정

--- src/test/java/leets/leets_mate/domain/MemberTests.java
+++ src/test/java/leets/leets_mate/domain/MemberTests.java
@@ -1,32 +1,25 @@
 package leets.leets_mate.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberTests {
-    private Member member;
-
-    @BeforeEach
-    void setUp() {
-        member = new Member();
-    }
 
     @Test
     void 입력받은_문자열을_파싱하여_리스트로_만든다() {
-        String members = "리츠에,오신,걸,환영합니다";
-        List<String> actual = member.parseMembers(members);
-        assertThat(actual).containsExactly("리츠에", "오신", "걸", "환영합니다");
-    }
+        String[] members = {"리츠에", "오신", "걸", "환영합니다"};
+        List<Member> actual = Member.parseMembers(members);
+        assertThat(actual).extracting(Member::getName)
+                .containsExactly("리츠에", "오신", "걸", "환영합니다");    }
 
     @Test
     void 멤버수를_반환한다() {
-        List<String> members = Arrays.asList("리츠에", "오신", "걸", "환영합니다");
-        int actual = member.memberNumber(members);
+        String[] membersArray = {"리츠에", "오신", "걸", "환영합니다"};
+        List<Member> members = Member.parseMembers(membersArray);
+        int actual = Member.memberNumber(members);
         assertThat(actual).isEqualTo(4);
     }
 }

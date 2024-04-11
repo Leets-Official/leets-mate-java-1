2babb5ee130f43f40d67fd8623cc95246124e11d
DongHoon Lee <dhl1924@naver.com>
2024-04-11 09:43:09 +0900

    feat(Member): 문자열로된 멤버들을 리스트로 분리하고, 총 멤버수를 반환하는 도메인 생성

---
+++ src/main/java/leets/leets_mate/domain/Member.java
@@ -0,0 +1,16 @@
+package leets.leets_mate.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Member {
+    // 문자열로된 멤버들을 리스트로 분리하는 함수입니다.
+    public List<String> parseMembers(String members) {
+        return new ArrayList<>();
+    }
+
+    // 총 멤버수를 반환합니다.
+    public int memberNumber(List<String> members) {
+        return 0;
+    }
+}

91094ac44edad460438f9ad6972703a4c71bd07b
DongHoon Lee <dhl1924@naver.com>
2024-04-11 11:17:36 +0900

    feat(IllegalArgumentExceptionType): 상수 추가

--- src/main/java/leets/leets_mate/view/exception/IllegalArgumentExceptionType.java
+++ src/main/java/leets/leets_mate/view/exception/IllegalArgumentExceptionType.java
@@ -3,7 +3,8 @@ package leets.leets_mate.view.exception;
 public enum IllegalArgumentExceptionType implements ExceptionType<IllegalArgumentException> {
 
     INVALID_NAMING("[ERROR] 이름은 한글로 입력해야 합니다"),
-    GROUP_SIZE_EXCEEDS_MEMBER_COUNT("[ERROR] 최대 짝 수는 이름의 갯수보다 클 수 없습니다.");
+    GROUP_SIZE_EXCEEDS_MEMBER_COUNT("[ERROR] 최대 짝 수는 이름의 갯수보다 클 수 없습니다."),
+    INVALID_MAX_GROUP("[ERROR] 최대 짝수는 숫자로 입력해야 합니다");
 
     private final String message;
 

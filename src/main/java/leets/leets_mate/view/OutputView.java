1ed6286ffb70052ad14fd317375d9c47a52fa335
DongHoon Lee <dhl1924@naver.com>
2024-04-11 11:19:11 +0900

    refactor(OutputView): static 키워드 추가
    - controller에서 사용하기 위한 static 추가

--- src/main/java/leets/leets_mate/view/OutputView.java
+++ src/main/java/leets/leets_mate/view/OutputView.java
@@ -3,7 +3,7 @@ package leets.leets_mate.view;
 import java.util.List;
 
 public class OutputView {
-    public void printStartMessage() {
+    public static void printStartMessage() {
         System.out.println("[Leets 오늘의 짝에게]를 시작합니다.");
         System.out.println();
         System.out.println("멤버의 이름을 입력해 주세요. (, 로 구분)");
